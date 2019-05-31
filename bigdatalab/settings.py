3738cdf45e361280da812f5f8d126285eb92cb09
Dmitry Golubkov <retmas@gmail.com>
2019-06-01 01:36:45 +0300

    Updated template settings

--- bigdatalab/settings.py
+++ bigdatalab/settings.py
@@ -22,8 +22,6 @@ SECRET_KEY = '*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -53,6 +51,7 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
+            'debug': DEBUG,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
